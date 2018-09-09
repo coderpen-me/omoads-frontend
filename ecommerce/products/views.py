9c436c13037ef1d5ae93d0beff506177e3d0fe3b
sidd5sci <sidd5sci@gmail.com>
2018-09-09 16:07:08 +0530

    faq

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -158,6 +158,25 @@ def directions(request):
 				}
 	return render(request, template, context)
 
+def faq_new(request):
+	template = "single-faq.html"
+	username = ""
+	userType = ""
+	if request.user.is_authenticated():
+		username = request.user.username
+		try:
+			Agency.objects.get(user = request.user)
+			userType = "Agency"
+		except Agency.DoesNotExist:
+			userType = "Buyer"
+	context = {
+				'loginStatus':request.user.is_authenticated(),
+				'username':username,
+				'userType':userType,
+				'faqid':1
+				}
+	return render(request, template, context)
+
 def faq(request):
 	template = "faqs.html"
 	username = ""
